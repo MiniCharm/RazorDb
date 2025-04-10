1ce08283042c88380ee624434166930a7aa3d9fe
MiniCharm <Johannedehn@gmail.com>
2025-04-10 12:16:58 +0200

    Update Secret.cs
    deleted secret

--- RazorDb/Services/Secret.cs
+++ RazorDb/Services/Secret.cs
@@ -1,17 +1,1 @@
-﻿namespace RazorDb.Services
-{
-    public class Secret
-    {
-        //Denne klasse, skal ikke deles på git-hub, eftersom dennne vil give adgang til databasen.
-        private static string _connectionString = 
-
-        public static string ConnectionString
-        {
-            get { return _connectionString; }
-
-        }
-        private static string quereyString = "SELECT Room_No,Hotel_No,Types,Price from Room";
-    }
-
-}
 
